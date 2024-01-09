8f405b827fdf0fc096a0f6ac432dcfc614cba567
lasir3 <lasir3@gmail.com>
2024-01-09 11:10:44 +0900

    change name / fix syntex error / add envCheck

--- shell/prop_install.sh
+++ shell/prop_install.sh
@@ -4,15 +4,15 @@ TEST_FLAG=1
 CONF_FILE=$1
 GOLDILOCKS_PROPERTY=$GOLDILOCKS_DATA/conf/goldilocks.property.conf
 
-echo "+----------------------------------+"
-echo "¦ Goldilocks config installation.  ¦"
-echo "+----------------------------------+"
+echo "+------------------------------------+"
+echo "¦ Goldilocks property installation.  ¦"
+echo "+------------------------------------+"
 echo ""
 
 failExit() {
-    echo "+---------------------------------------+"
-    echo "¦ !!!! Config installation failed. !!!! ¦"
-    echo "+---------------------------------------+"
+    echo "+-----------------------------------------+"
+    echo "¦ !!!! property installation failed. !!!! ¦"
+    echo "+-----------------------------------------+"
     echo ""
     exit
 }
@@ -20,6 +20,8 @@ failExit() {
 envCheck(){
     if [ -z "$GOLDILOCKS_DATA" ]; then
         echo "Error: $GOLDILOCKS_DATA env is not set." >&2
+        echo "Did you Install Goldilocks package?"
+        echo ""
         failExit
     fi
 }
@@ -27,7 +29,7 @@ envCheck(){
 addProperty() {
     ## if CONF_FILE don't exist
     if [ ! -f "$CONF_FILE" ]; then
-        echo "Config file does not exist."
+        echo "config file does not exist."
         echo "Check the file and try again."
         echo "How to use : $(basename $0) package_name.tar.gz goldilocks.conf license" >&2
         failExit
@@ -43,13 +45,15 @@ addProperty() {
         fi
     fi
 }
+### CHECK ENV
+envCheck
 
 ### ADD PROPERTY
 if [ $TEST_FLAG -eq 1 ]; then
     addProperty
 fi
 
-echo "+--------------------------------------------+"
-echo "¦ Goldilocks license installation finished.  ¦"
-echo "+--------------------------------------------+"
+echo "+---------------------------------------------+"
+echo "¦ Goldilocks property installation finished.  ¦"
+echo "+---------------------------------------------+"
 echo ""
